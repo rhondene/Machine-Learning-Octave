deac11b3a1d7c91cd566b006bc31ff998b9e53af
Rhondene_fminaunt <rhondenewint93@gmail.com>
2017-12-09 14:52:12 -0800

    Delete PolynomialFeatures.m

--- PolynomialFeatures.m
+++
@@ -1,9 +0,0 @@
-%maps X (mx1) feature vector to a data matirx X_poly (m x p) containing polynomial of each feature all the wat to the p-th power
-
-X_poly = zeros(numel(X), p);        %numel returns the number of elements in array X
-
-for k = 1:p,
-  X_poly(:,k) = X.^k;
-end
-
-    
